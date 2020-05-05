21003a720997b658671b895f67994dc4d5974cc3
Eymric <gatibelza.eymric@gmail.com>
2020-05-05 18:15:09 +0200

    [Changes] io

--- io/io.js
+++ io/io.js
@@ -1,5 +1,19 @@
+const fs = require('fs');
+
 module.exports.decodeHexFileContent = (filePath) =>  {
+
     return new Promise((resolve, reject) => {
         // To be implemented!
+        let stream = fs.createReadStream(filePath);
+        stream.on("data", function(data) {
+            let chunk = data.toString();
+            const convert = (from, to) => str => Buffer.from(str, from).toString(to)
+            const hexToUtf8 = convert('hex', 'utf8');
+            let res = hexToUtf8(chunk);
+            resolve(res);
+        });
+        stream.on("error", function(data) {
+            reject(data);
+        });
     });
-}
+};
